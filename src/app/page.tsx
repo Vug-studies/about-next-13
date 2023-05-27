be1c7ae5412899e8b5dc75d2b9b5f7c8c0523417
namevug <ciavug@gmail.com>
2023-05-26 22:22:24 -0300

    feat: Layouts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import Link from 'next/link'
+
 export default function Home() {
-  return <h1>Olá vug</h1>
+  return (
+    <div>
+      <h1>Olá vug</h1>
+      <Link href={'/app'}>Dashboard</Link>
+    </div>
+  )
 }
