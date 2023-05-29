f87877adfea0ef0226aca820a43a8fcc0ecdd900
namevug <ciavug@gmail.com>
2023-05-29 12:41:32 -0300

    feat: static generation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,25 @@
 import Link from 'next/link'
 
-export default function Home() {
+// revalidate
+export const revalidate = 30
+
+export default async function Home() {
+  const response = await fetch('https://api.github.com/users/namevug', {
+    next: {
+      revalidate: 30,
+    },
+  })
+
+  const user = await response.json()
+
   return (
     <div>
       <h1>Olá vug</h1>
+      <pre>{JSON.stringify(user, null, 2)}</pre>
       <Link href={'/app'}>Dashboard</Link>
     </div>
   )
 }
+
+// revalidate
+// SSG
